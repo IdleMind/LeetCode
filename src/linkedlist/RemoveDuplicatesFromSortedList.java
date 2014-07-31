cce4b536ebe8cbe02dd4ba462e0c9ef6dce79523
zhoutai <zhoutai603@gmail.com>
2014-07-31 15:31:10 +0800

    remove duplicates from sorted list

---
+++ src/linkedlist/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,72 @@
+package linkedlist;
+
+/**
+  @author     Tai Zhou, zhoutai603@gmail.com
+  Date:       Jul 31, 2014
+  
+  Problem:    Remove Duplicates from Sorted List
+  Source:     https://oj.leetcode.com/problems/remove-duplicates-from-sorted-list/
+  Difficulty: Easy
+  
+  Key Idea:   two pointers
+*/
+
+public class RemoveDuplicatesFromSortedList {
+  public static class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+      val = x;
+      next = null;
+    }
+  }
+  
+  public static class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+      if (head == null) {
+        return null;
+      }
+      
+      ListNode curValue = head;
+      ListNode nextValue = head.next;
+      while (curValue != null) {
+        while (nextValue != null && nextValue.val == curValue.val) {
+          nextValue = nextValue.next;
+        }
+        
+        curValue.next = nextValue;
+        curValue = nextValue;
+      }
+      
+      return head;
+    }
+  }
+  
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    
+    ListNode head = new ListNode(1);
+    traverseList(head);
+    s.deleteDuplicates(head);
+    traverseList(head);
+    
+    head.next = new ListNode(1);
+    head.next.next = new ListNode(2);
+    traverseList(head);
+    s.deleteDuplicates(head);
+    traverseList(head);
+  }
+  
+  private static void traverseList(ListNode head) {
+    if (head == null) {
+      System.out.println("Empty list...");
+    } else {
+      ListNode curNode = head;
+      while (curNode != null) {
+        System.out.print(curNode.val + " ");
+        curNode = curNode.next;
+      }
+      System.out.println();
+    }
+  }
+}
