889a629b114d0341de7f35193c99cd8022ae295f
zhoutai <zhoutai603@gmail.com>
2014-08-01 21:09:03 +0800

    flatten binary tree to linked list {moderate}

---
+++ src/tree/FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,68 @@
+package tree;
+
+/**
+  @author     Tai Zhou, zhoutai603@gmail.com
+  Date:       Aug 1, 2014
+  
+  Problem:    Flatten Binary Tree to Linked List
+  Source:     https://oj.leetcode.com/problems/flatten-binary-tree-to-linked-list/
+  
+  Key Idea:   DFS (pre-order traversal)
+*/
+
+public class FlattenBinaryTreeToLinkedList {
+  public static class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+  
+  public static class Solution {
+    public static class TreeNodeRef {
+      TreeNode list;
+    }
+    
+    public void flatten(TreeNode root) {
+      if (root == null) {
+        return;
+      }
+      
+      TreeNodeRef list = new TreeNodeRef();
+      flattenInternal(root, list);
+      root = list.list;
+    }
+    
+    private void flattenInternal(TreeNode root, TreeNodeRef list) {
+      if (root.right != null) {
+        flattenInternal(root.right, list);
+      }
+      
+      if (root.left != null) {
+        flattenInternal(root.left, list);
+      }
+      
+      root.right = list.list;
+      root.left = null;
+      list.list = root;
+    }
+  }
+  
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(2);
+    root.right = new TreeNode(3);
+   
+    s.flatten(root);
+    
+    while (root != null) {
+      System.out.println(root.val + " ");
+      root = root.right;
+    }
+  }
+}
